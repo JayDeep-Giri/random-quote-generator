21498013c8464fa29dea46db9c0d50b22e1c512b
JayDeep-Giri <jaydeepgiri30@gmail.com>
2023-11-04 18:00:34 +0530

    Added hooks and Api

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect, useRef } from 'react';
+import './App.css';
+
+function App() {
+  const [quotes, setQuotes] = useState('');
+  const textRef = useRef();
+  let colors = ["#ffff00", "#90ee90", "#ffa500", "#ff68ff", "#a9a9e7"];
+
+  const getQuote = () => {
+    fetch("https://type.fit/api/quotes")
+    .then(res => res.json())
+    .then(data => {
+      let randomNum = Math.floor(Math.random() * data.length);
+      setQuotes(data[randomNum]);
+    })
+  };
+
+  useEffect(() => {
+    getQuote();
+  }, []);
+
+  useEffect(() => {
+    textRef.current.style.color =
+     colors[Math.floor(Math.random() * colors.length)];
+  }, [quotes]);
+
+  return (
+    <div className='App'>
+      <div className='quote'>
+        <p ref={textRef}>{quotes.text}</p>
+        <p>Author: {quotes.author}</p>
+        <div className='btnContainer'>
+          <button onClick={getQuote} className='btn'>Get Quote</button>
+          <a
+           href={`https://linkedin.com/intent/feed?text=${quotes.text}`} className='btn'>
+            Tweet
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
